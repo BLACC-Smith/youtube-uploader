5961d606bd368ed8e745d65466ff84729b811a3a
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-01-03 23:53:10 -0600

    added discord rerouting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import Home from './components/organisms/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Reroute from './components/organisms/Reroute';
 
 function App() {
-	return <Home />;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/discord">
+					<Reroute />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
