6b52f4706527484363320a35282b95a7bc1e08f9
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-01-04 01:52:29 -0600

    removed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import Home from './components/organisms/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Reroute from './components/organisms/Reroute';
 
 function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/discord">
-					<Reroute />
-				</Route>
-			</Switch>
-		</Router>
-	);
+	return <Home />;
 }
 
 export default App;
