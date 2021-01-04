3bce27056e17a1398e216ca460e1f7b03742dfa5
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-01-04 01:45:46 -0600

    added netlify redirects

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
 	return (
 		<Router>
 			<Switch>
-				<Route path="/discord">
-					<Reroute />
-				</Route>
 				<Route exact path="/">
 					<Home />
+				</Route>
+				<Route path="/discord">
+					<Reroute />
 				</Route>
 			</Switch>
 		</Router>
